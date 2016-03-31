4bd68bd68a96473ec32ea06516f3c0dc942b5a55
anthony <anthony@LenovoUbuntu>
2016-03-31 12:24:51 -0500

    Second Push to MyProjects Repository
    This commit and push contains the beginnings of my server/client code
    which will be worked on it the future.

---
+++ MyProjects/src/Networking/client.java
@@ -0,0 +1,17 @@
+package Networking;
+
+//Anthony Snyder
+//March 31, 2016
+
+//This client class contains code that will be used as the basis for
+//networking clients in the future and can be modified to serve a specific
+//purpose.
+public class client {
+	public client(){
+		
+	}
+	
+	public static void main(String args[]){
+		
+	}
+}
---
+++ MyProjects/src/Networking/server.java
@@ -0,0 +1,17 @@
+package Networking;
+
+//Anthony Snyder
+//March 31, 2016
+
+//This server class will contain the code for my server/client
+//program.  It will be basic, working server that can be changed
+//to be used in any number of ways.
+public class server {
+	public server(){
+		
+	}
+	
+	public static void main(String args[]){
+		
+	}
+}
